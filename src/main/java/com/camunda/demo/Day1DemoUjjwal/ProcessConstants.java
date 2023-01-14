cb33b21d7b88ee56dba62309837783b0ba798a0c
Ujjwal Tyagi <tyagi004ujjwal@gmail.com>
2023-01-14 20:53:06 +0530

    1c

---
+++ src/main/java/com/camunda/demo/Day1DemoUjjwal/ProcessConstants.java
@@ -0,0 +1,7 @@
+package com.camunda.demo.Day1DemoUjjwal;
+
+public class ProcessConstants {
+
+  public static final String PROCESS_DEFINITION_KEY = "Day1DemoUjjwal"; // BPMN Process ID
+
+}
---
+++ src/main/java/com/camunda/demo/Day1DemoUjjwal/printMsg.java
@@ -0,0 +1,23 @@
+package com.camunda.demo.Day1DemoUjjwal;
+
+import java.util.Random;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class printMsg implements JavaDelegate {
+
+	@Override
+	public void execute(DelegateExecution execution) throws Exception {
+		System.out.println(" -------------------- hey -------------------------\n");
+		
+		Random rnd = new Random();
+		boolean boo ;
+		//= rnd.nextBoolean();
+		System.out.println("testing");
+		
+		execution.setVariable("boo",rnd.nextBoolean());
+		
+	}
+
+}
